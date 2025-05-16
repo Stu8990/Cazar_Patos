8c69e23de7ffa9adfca52efa018baf276be043fc
Stuart Palma <jhou2013x@hotmail.com>
2025-05-16 15:15:30 -0500

    Screen login created

---
+++ app/src/main/java/com/stuart/palma/cazarpatos/Login.kt
@@ -0,0 +1,26 @@
+package com.stuart.palma.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun openMain(view: View){
+        val intent= Intent(this , MainActivity::class.java)
+        startActivity(intent)
+    }
+}
